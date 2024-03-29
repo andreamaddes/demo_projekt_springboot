e66d2ad167361826c85f9eaf69aa0b4925385b24
andreamaddes <andreamaddes@gmail.com>
2024-03-29 12:46:34 +0100

    Person

--- src/main/java/ch/zhaw/springboot/demo_projekt_springboot/PersonController.java
+++ src/main/java/ch/zhaw/springboot/demo_projekt_springboot/PersonController.java
@@ -1,15 +1,30 @@
 package ch.zhaw.springboot.demo_projekt_springboot;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 
 public class PersonController {
-    @GetMapping ("/test")
-    public String test() {
-    return "Hello World app is up and running!";
+
+    private Map<Integer, Person> persons = new HashMap<>();
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init () {
+      this.persons.put(1,new Person(1, "Barack Obama"));
+      this.persons.put(2,new Person(2, "Donald Trum"));
+      this.persons.put(3,new Person(3, "Andrea Maddes"));
+    System.out.println("Init Data");
     }
 
+@GetMapping("/person/{id}")
+public Person getPerson (@PathVariable Integer id){
+    return this.persons.get(id);
+}
 
 }
