9d1f9b8be878b19e1ed44219d18dd6d3275a5252
andreamaddes <andreamaddes@gmail.com>
2024-03-29 10:40:00 +0100

    publish

---
+++ src/main/java/ch/zhaw/springboot/demo_projekt_springboot/DemoProjektSpringbootApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.springboot.demo_projekt_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjektSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjektSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/springboot/demo_projekt_springboot/DemoProjektSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.springboot.demo_projekt_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjektSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
