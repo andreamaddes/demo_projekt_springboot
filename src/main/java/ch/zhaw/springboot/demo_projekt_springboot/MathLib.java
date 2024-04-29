acf5de8bae564f980d11b3b94bb06be24d7b9ed0
andreamaddes <andreamaddes@gmail.com>
2024-04-29 09:59:06 +0100

    Mathlibtests

--- src/main/java/ch/zhaw/springboot/demo_projekt_springboot/MathLib.java
+++ src/main/java/ch/zhaw/springboot/demo_projekt_springboot/MathLib.java
@@ -1,3 +1,5 @@
+package ch.zhaw.springboot.demo_projekt_springboot;
+
 public class MathLib {
     public static boolean isEven(int value) {
         return value % 2 == 0;
--- src/test/java/ch/zhaw/springboot/demo_projekt_springboot/MathlibTest.java
+++ src/test/java/ch/zhaw/springboot/demo_projekt_springboot/MathlibTest.java
@@ -1,7 +1,9 @@
+package ch.zhaw.springboot.demo_projekt_springboot;
+
 import static
 org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-public class MathLibTest {
+public class MathlibTest {
 
     @Test
     public void testEven1() {
